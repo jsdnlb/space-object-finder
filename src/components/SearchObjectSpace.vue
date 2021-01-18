027815a8fd35e2dddd6f37326b2b752a49d7601f
jsdnlb <danielb.alzate1@gmail.com>
2021-01-18 10:32:13 -0500

    Corrigiendo bug con axios

--- src/components/SearchObjectSpace.vue
+++ src/components/SearchObjectSpace.vue
@@ -8,27 +8,13 @@
           <p>{{ examples }}</p>
 
           <form v-on:submit.prevent="getResult(query)">
-            <v-text-field
-              type="text"
-              v-model="query"
-              label="Ingresa una palabra curiosa"
-            />
+            <v-text-field type="text" v-model="query" label="Ingresa una palabra curiosa" />
           </form>
         </v-col>
       </v-row>
       <div v-if="results" class=" d-md-flex">
-        <v-card
-          class="d-flex align-content-start flex-wrap justify-center"
-          flat
-          tile
-        >
-          <v-card
-            v-for="result in results"
-            :key="result.id"
-            class="pa-2 ma-2"
-            outlined
-            tile
-          >
+        <v-card class="d-flex align-content-start flex-wrap justify-center" flat tile>
+          <v-card v-for="result in results" :key="result.id" class="pa-2 ma-2" outlined tile>
             <img v-bind:src="result.links[0].href" class="imgs" height="200" />
           </v-card>
         </v-card>
@@ -47,10 +33,8 @@ export default {
   data() {
     return {
       title: "¿Qué deseas buscar? ",
-      subtitle:
-        "Este es un simple buscador de imágenes de la NASA, sólo debes escribir una palabra para que se realice una búsqueda relacionada a su solicitud. Por el momento sólo se aceptan palabras en Inglés, pero eventualmente se implementará el idioma Español.",
-      examples:
-        "Trate de usar palabras clave, como nombres de planetas, galaxias, estrellas, misiones espaciales, astronautas, científicos astrónomos entre otros... Algunos ejemplos:{ Jupiter, Neptune, Moon, Sun, Demo-crew , Apollo 11, Neil Armstrong, Valentina Tereshkova, Elon Musk, Carl Sagan, Andromeda, Quasar } ",
+      subtitle: "Este es un simple buscador de imágenes de la NASA, sólo debes escribir una palabra para que se realice una búsqueda relacionada a su solicitud. Por el momento sólo se aceptan palabras en Inglés, pero eventualmente se implementará el idioma Español.",
+      examples: "Trate de usar palabras clave, como nombres de planetas, galaxias, estrellas, misiones espaciales, astronautas, científicos astrónomos entre otros... Algunos ejemplos:{ Jupiter, Neptune, Moon, Sun, Demo-crew , Apollo 11, Neil Armstrong, Valentina Tereshkova, Elon Musk, Carl Sagan, Andromeda, Quasar } ",
       query: "",
       results: [],
     };
@@ -67,14 +51,10 @@ export default {
           Swal.showLoading();
         },
       });
-      axios
-        .get(
-          "https://images-api.nasa.gov/search?q=" + query + "&media_type=image"
-        )
-        .then((response) => {
-          console.log("getResult -> response", response.data.collection.items);
-          this.results = response.data.collection.items;
-        });
+      axios.get("https://images-api.nasa.gov/search?q=" + query + "&media_type=image").then((response) => {
+        // console.log("getResult -> response", response.data.collection.items);
+        this.results = response.data.collection.items;
+      });
       Swal.close();
     },
   },
